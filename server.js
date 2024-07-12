bee3b1703ece79aba8b863db07f26369e932a6e5
TheoDT2674 <theo.de_thomasis@ensam.eu>
2024-07-12 10:42:55 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+
+const stripe = require("stripe")(
+  // This is your test secret API key.
+  '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',
+  {
+    apiVersion: "2023-10-16",
+  }
+);
+
+app.use(express.static("dist"));
+app.use(express.json());
+
+app.post("/account_link", async (req, res) => {
+  try {
+    const { account } = req.body;
+
+    const accountLink = await stripe.accountLinks.create({
+      account: account,
+      return_url: `${req.headers.origin}/return/${account}`,
+      refresh_url: `${req.headers.origin}/refresh/${account}`,
+      type: "account_onboarding",
+    });
+
+    res.json(accountLink);
+  } catch (error) {
+    console.error(
+      "An error occurred when calling the Stripe API to create an account link:",
+      error
+    );
+    res.status(500);
+    res.send({ error: error.message });
+  }
+});
+
+app.post("/account", async (req, res) => {
+  try {
+    const account = await stripe.accounts.create({
+      controller: {
+        stripe_dashboard: {
+          type: "express",
+        },
+        fees: {
+          payer: "application"
+        },
+        losses: {
+          payments: "application"
+        },
+      },
+    });
+
+    res.json({
+      account: account.id,
+    });
+  } catch (error) {
+    console.error(
+      "An error occurred when calling the Stripe API to create an account",
+      error
+    );
+    res.status(500);
+    res.send({ error: error.message });
+  }
+});
+
+app.get("/*", (_req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
+app.listen(4242, () => console.log("Node server listening on port 4242! Visit http://localhost:4242 in your browser."));
